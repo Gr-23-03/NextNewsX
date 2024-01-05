949644324a6fe625ed63ff8b8488ec99d99be847
Zain ul Abedeen <zaini7413@gmail.com>
2024-01-05 09:58:32 +0100

    Subscription view is added

---
+++ NextNews/Controllers/SubscriptionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NextNews.Controllers
+{
+    public class SubscriptionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
