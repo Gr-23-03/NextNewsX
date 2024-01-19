23a1c758213a902bd46ed7009c9082c2eb97b18f
Violetmuk <mukisaviolet@gmail.com>
2024-01-19 13:22:28 +0100

    front page design.

--- NextNews/ViewModels/HomeIndexVM.cs
+++ NextNews/ViewModels/HomeIndexVM.cs
@@ -12,5 +12,19 @@ namespace NextNews.ViewModels
         public List<LatestNewsViewModel>? LatestArticles { get; set; }
 
         public Article LatestArticle { get; set; }
+
+
+
+
+        public List<Category>? AllCategories { get; set; }
+
+        public List<Article>? ArticlesByCategorySport { get; set; } // Added property for category-specific articles
+
+        public List<Article>? ArticlesByCategoryWorld { get; set; } // Added property for category-specific articles
+
+        public Article SpecificArticle { get; set; }
+
+        public List<Article>? SpecificArticles { get; set; }
+
     }
 }
