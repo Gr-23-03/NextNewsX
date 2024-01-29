0fc303153f3e52226c710295261cee3e77f53e90
Esri Hazael Juarez <esri.juarez@outlook.com>
2024-01-29 13:50:44 +0100

    Backgrounds

--- NextNews/Controllers/CategoriesViewController.cs
+++ NextNews/Controllers/CategoriesViewController.cs
@@ -25,10 +25,40 @@ namespace NextNews.Controllers
         public IActionResult ArticlesByCategories(int categoryId)
         {
             var articles = _articleService.GetArticlesByCategory(categoryId);
+            var category = _categoryService.GetCategoryById(categoryId);
 
-            @ViewBag.CategoryName = _categoryService.GetCategoryById(categoryId).Name;
+            @ViewBag.CategoryName = category.Name;
+            @ViewBag.BackgroundImageUrl = GetBackgroundImageUrlForCategory(category.Name);
 
             return View(articles);
+        }
+        private string GetBackgroundImageUrlForCategory(string categoryName)
+        {
+            // Here, map category names to their respective image URLs
+            switch (categoryName.ToLower())
+            {
+                case "business":
+                    return "/Images/Business.jpg";
+                case "health":
+                    return "/Images/health1.jpg";
+                case "local":
+                    return "/Images/Visit_PR-flygbild.jpg";
+                case "art & culture":
+                    return "/Images/museum-background.jpg";
+                case "sport":
+                    return "/Images/sport.jpg";
+                case "sweden":
+                    return "/Images/Sweden.jpeg";
+                case "weather":
+                    return "/Images/weather.jpg";
+                case "world":
+                    return "/Images/world.jpg";
+                case "entertainment":
+                    return "/Images/Entertainment.jpg";
+
+                default:
+                    return "default-background.jpg";
+            }
         }
 
     }
