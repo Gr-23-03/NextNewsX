a62be13df765186f7804b17370ed7420305d87c4
Zain ul Abedeen <zaini7413@gmail.com>
2024-02-15 14:19:52 +0100

    Subscription has been modified

--- NextNews/Controllers/ContactController.cs
+++ NextNews/Controllers/ContactController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
 using NextNews.Models.Database;
 using NextNews.Services;
 
@@ -8,10 +9,11 @@ namespace NextNews.Controllers
     public class ContactController : Controller
     {
         private readonly IContactService _contactService;
-
-        public ContactController(IContactService contactService)
+        private readonly IEmailSender _emailSender;
+        public ContactController(IContactService contactService, IEmailSender emailSender)
         {
                 _contactService = contactService;
+            _emailSender = emailSender;
         }
 
 
@@ -31,12 +33,18 @@ namespace NextNews.Controllers
         [HttpPost]
         public ActionResult SaveContactFormMessage(ContactFormMessage obj)
         {
+
             if (ModelState.IsValid)
             {
                 // Process the form data (e.g., send an email, save to a database).
                 _contactService.SaveContactMessage(obj);
 
                 // For this example, let's just return a view with the submitted data.
+                string htmlTemplate = System.IO.File.ReadAllText("C:\\Users\\zain\\source\\repos\\NextNews\\NextNews\\Views\\Shared\\EmailTemplate.cshtml");
+                string personalizedContent = $"Thank you for giving us feedback.We will contanct you soon. The NextNews takes your data privacy seriously. To learn more, read our privacy policy and account FAQs.All the best, Regards NextNews";
+                string htmlMessage = htmlTemplate.Replace("{{main_content}}", personalizedContent);
+                _emailSender.SendEmailAsync(obj.Email, "NextNews Subscription", htmlMessage);
+             
                 return View("ThankYouMessage");
             }
             else
